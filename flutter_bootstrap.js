9816102def61c0f3705c83a49051fe944e2f480c
yukuai0011 <kuyu5570@uni.sydney.edu.au>
2025-10-29 14:12:23 +0800

    feat(pages): add service worker, enable registration, and set <base href> to '/'
    - Add generated flutter_service_worker.js to enable offline caching.
    - Enable service worker registration in flutter_bootstrap.js (serviceWorkerVersion "1469749612").
    - Simplify index.html by setting <base href> to "/" and removing the dynamic base-href script.
    
    Signed-off-by: yukuai0011 <kuyu5570@uni.sydney.edu.au>

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -36,4 +36,8 @@ if (!window._flutter) {
 }
 _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7ca","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
-_flutter.loader.load();
+_flutter.loader.load({
+  serviceWorkerSettings: {
+    serviceWorkerVersion: "1469749612"
+  }
+});
